78401d676618040d61d86f24e2cc69aadce7d846
Gino-Y <ginoyan250@gmail.com>
2021-09-17 17:13:22 +0800

    add

--- 001-TypeScript简介和环境搭建.ts
+++ 001-TypeScript简介和环境搭建.ts
@@ -7,3 +7,25 @@
 
 // $ tsc -v
 // Version 3.2.2
+
+
+const  hello:string = 'hello world!'
+console.log(hello)
+
+function jspang(){
+    let hello:string = 'hello world!'
+    console.log(hello)
+}
+
+jspang()
+
+
+//------ terminal运行--------//
+
+// tsc 001-TypeScript简介和环境搭建.ts
+// 生成.js文件，并保存到文件
+//
+// ts-node
+// npm install -g ts-node
+// ts-node 001-TypeScript简介和环境搭建.ts
+// 内部转换js文件直接运行
---
+++ 002-TypeScript静态类型.ts
@@ -0,0 +1,14 @@
+let count:number = 1
+
+// 自定义静态类型
+interface  Xiaojiejie{
+    uname:string,
+    age:number
+}
+
+const xiaohong:Xiaojiejie = {
+    uname:'小红',
+    age:18
+}
+
+console.log(xiaohong.age)
