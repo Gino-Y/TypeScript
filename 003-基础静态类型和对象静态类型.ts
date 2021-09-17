39ca9d18bd616a5a1c4a572b8aad1de504541c3c
Gino-Y <ginoyan250@gmail.com>
2021-09-17 18:12:29 +0800

    add

---
+++ 003-基础静态类型和对象静态类型.ts
@@ -0,0 +1,19 @@
+const count:number = 654;
+const myName:string = 'Gino';
+// null, undefinde, boolean, void, symbol
+
+const xiaoJieJie:{
+    name:string,
+    age:number
+} = {
+    name:'小红',
+    age:18
+}
+
+const xiaoJieJies:string[] = ['Gino', 'Tina', 'Luna']
+
+class Person{}
+const dajie:Person = new Person()
+
+const jianXiaoJieJie:()=>string=()=>{return '大姐'}
+// 对象类型 数组类型 类类型 函数类型
---
+++ 004-类型注解和类型推断.ts
@@ -0,0 +1,22 @@
+// 类型推断
+let countInterence = 123;
+
+const one = 1;
+const tow = 2;
+const three = one + tow;
+
+const SuaiGe = {
+    name:'Gino',
+    age:18
+}
+
+
+// 类型注解
+let count : number;
+count = 123;
+
+function getTotal(one : number, tow : number){
+    return one + tow
+}
+const total = getTotal(1,2)
+
