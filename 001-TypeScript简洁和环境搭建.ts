ad738f683223ef4618d688537a7a8a1aec08a659
Gino-Y <ginoyan250@gmail.com>
2021-09-17 15:51:19 +0800

    add

--- 001-TypeScript简洁和环境搭建.ts
+++ 001-TypeScript简洁和环境搭建.ts
@@ -1,2 +1,9 @@
 // https://www.bilibili.com/video/BV1qV41167VD?from=search&seid=3318125639877619960&spm_id_from=333.337.0.0
+// https://www.runoob.com/typescript/ts-install.html
 
+// npm config set registry https://registry.npm.taobao.org
+//
+// npm install -g typescript
+
+// $ tsc -v
+// Version 3.2.2
